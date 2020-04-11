2298b8f33f8c4044b0de10dd8cd8db2a9af42995
William Bruntrager <w.bruntrager@gmail.com>
2020-04-11 13:35:25 +0200

    Updates

---
+++ precache-manifest.80c8863136871cc784258a6418f5df3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d556aefbfd8d74996a85398aaa1d627",
+    "url": "/cantstop/index.html"
+  },
+  {
+    "revision": "ec94e3410ade83cb7db7",
+    "url": "/cantstop/static/css/main.9ed12d5d.chunk.css"
+  },
+  {
+    "revision": "6fb8e18725463b11016e",
+    "url": "/cantstop/static/js/2.563a9874.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/cantstop/static/js/2.563a9874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec94e3410ade83cb7db7",
+    "url": "/cantstop/static/js/main.ea525396.chunk.js"
+  },
+  {
+    "revision": "baa8732d16be38b5f1b7",
+    "url": "/cantstop/static/js/runtime-main.2b243b6e.js"
+  }
+]);
